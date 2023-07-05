50a32e8c4420aad1f96cb1adbf68fea4b113275b
Moritz Thoma <moritz.thoma@twenty20.eu>
2023-07-05 18:48:54 +0200

    started doku

--- src/main/java/de/dhbw/compiler/Compiler.java
+++ src/main/java/de/dhbw/compiler/Compiler.java
@@ -9,7 +9,20 @@ import de.dhbw.compiler.typecheck.TypeException;
 
 public class Compiler {
     public static void main(String[] args) throws SyntaxException, TypeException {
-        Program p = Parser.parse("class NiceClass {}");
+        Program p = Parser.parse(
+            """
+class NiceClass {
+    int i;
+
+    public NiceClass(){}
+
+    public int do(){
+        i = 10 ;
+        return i;
+    }
+}
+    """);
+
         TypeCheck t = new TypeCheck();
         p = t.check(p);
         CodeGenerator.generateCode(p);
