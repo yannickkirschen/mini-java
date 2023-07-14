a7eeac41566049cb232caed24a633e9ec56331e8
Rex2002 <39184634+Rex2002@users.noreply.github.com>
2023-07-14 15:46:15 +0200

    feat: add code input via args (#16)
    Co-authored-by: Rex2002 <benjamin.frahm@atos.net>

--- src/main/java/de/dhbw/compiler/Compiler.java
+++ src/main/java/de/dhbw/compiler/Compiler.java
@@ -7,18 +7,28 @@ import de.dhbw.compiler.typecheck.SyntaxException;
 import de.dhbw.compiler.typecheck.TypeCheck;
 import de.dhbw.compiler.typecheck.TypeException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Compiler {
-    public static void main(String[] args) throws SyntaxException, TypeException {
-        Program p = Parser.parse(
-"""
-class NiceClass {
-    void do() {
-        int v = 2;
-        return v;
-    }
-}
-"""
-        );
+    public static void main(String[] args) throws SyntaxException, TypeException, IOException {
+        String code;
+        if(args.length == 1){
+            code = Files.readString(Path.of(args[0]));
+        }
+        else{
+            code =
+                """
+                 class NiceClass {
+                    void do() {
+                        int v = 2;
+                        return v;
+                    }
+                }
+                """;
+        }
+        Program p = Parser.parse(code);
         TypeCheck t = new TypeCheck();
         p = t.check(p);
         CodeGenerator.generateCode(p);
